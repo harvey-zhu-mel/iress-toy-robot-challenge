25048ff4294f86fe9793e031a7ffcccf78964da5
harvey-zhu-mel <harvey545001@gmail.com>
2026-05-21 17:18:37 +1000

    feat: wire console app to simulator

--- src/Iress.ToyRobot.Console/Program.cs
+++ src/Iress.ToyRobot.Console/Program.cs
@@ -1,1 +1,45 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Iress.ToyRobot.Core;
+
+IEnumerable<string> inputLines;
+
+if (args.Length > 1)
+{
+    Console.Error.WriteLine("Usage: Iress.ToyRobot.Console [input-file]");
+    return 1;
+}
+
+if (args.Length == 1)
+{
+    var path = args[0];
+
+    if (!File.Exists(path))
+    {
+        Console.Error.WriteLine($"Input file not found: {path}");
+        return 1;
+    }
+
+    inputLines = File.ReadLines(path);
+}
+else
+{
+    inputLines = ReadStandardInput();
+}
+
+var simulator = new RobotSimulator();
+
+foreach (var output in simulator.Process(inputLines))
+{
+    Console.WriteLine(output);
+}
+
+return 0;
+
+static IEnumerable<string> ReadStandardInput()
+{
+    string? line;
+
+    while ((line = Console.ReadLine()) is not null)
+    {
+        yield return line;
+    }
+}
